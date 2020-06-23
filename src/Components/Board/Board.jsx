ff2a9fac9d83d90895f301d7c8852e0d55a6c2a7
Atanas Krondev <47859124+AtanasKrondev@users.noreply.github.com>
2020-06-23 15:58:56 +0300

    Update Board.jsx

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TaskList from '../Task/TaskList';
 import TaskListForm from '../Task/TaskListForm';
 import Container from '@material-ui/core/Container';
 import { useParams, useHistory } from 'react-router-dom';
-import { boardsRef } from '../../firebase';
-import { Typography, LinearProgress, IconButton, TextField, Box } from '@material-ui/core';
+import { boardsRef, usersRef, fieldValue } from '../../firebase';
+import { Typography, LinearProgress, IconButton, TextField, Box, Menu, MenuItem, ListItemIcon, ListItemText, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import CloseIcon from '@material-ui/icons/Close';
 import SaveIcon from '@material-ui/icons/Save';
 import { title } from '../../vaildators';
 import { Formik } from 'formik';
+import { UserContext } from '../../providers/UserProvider';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -44,6 +47,24 @@ export default function Board() {
     const history = useHistory();
     const [editTitle, setEditTitle] = useState(false);
     const handleEditTitle = () => setEditTitle(!editTitle);
+    const { user } = useContext(UserContext)
+
+    const deleteBoard = () => {
+        if (!board.lists.length) {
+            boardsRef.doc(id).delete()
+                .then(() => history.push('/home'))
+                .catch(error => console.error(error))
+            usersRef.doc(user.uid).set({ boards: fieldValue.arrayRemove(id) }, { merge: true })
+                .catch(error => console.error(error))
+        }
+    };
+
+    const [deleteDialog, setDeleteDialog] = useState(false);
+    const handleDeleteDialog = () => setDeleteDialog(!deleteDialog);
+
+    const [anchorEl, setAnchorEl] = useState(null)
+    const handleMenuOpen = (event) => { setAnchorEl(event.currentTarget) }
+    const handleMenuClose = () => setAnchorEl(null);
 
     useEffect(() => boardsRef.doc(id).onSnapshot(snapshot => {
         setLoading(true)
@@ -78,15 +99,39 @@ export default function Board() {
                         </form>)}
                 </Formik> :
                 <Typography component="h1" variant="h6">{board && board.title}
-                    <IconButton size="small" onClick={handleEditTitle}><EditIcon /></IconButton>
+                    <IconButton size="small" onClick={handleMenuOpen}><MoreVertIcon /></IconButton>
+                    <Menu anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                        onClick={handleMenuClose}>
+                        <MenuItem onClick={handleEditTitle}>
+                            <ListItemIcon><EditIcon /></ListItemIcon>
+                            <ListItemText primary="Edit" />
+                        </MenuItem>
+                        {board && board.lists && !board.lists.length &&
+                            (<MenuItem onClick={handleDeleteDialog}>
+                                <ListItemIcon><DeleteIcon /></ListItemIcon>
+                                <ListItemText primary="Delete" />
+                            </MenuItem>)}
+                    </Menu>
                 </Typography>}
         </Box>
         <Container className={classes.root}>
             {board.lists && board.lists
-                // .sort((a, b) => a.position - b.position)
                 .map(listId => (<TaskList key={listId} id={listId} boardId={id}></TaskList>))}
             < TaskListForm boardId={id} />
         </ Container></>}
+        <Dialog open={deleteDialog} onClose={handleDeleteDialog}>
+            <DialogTitle>Delete this board?</DialogTitle>
+            <DialogContent>
+                <DialogContentText>{board && board.title}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleDeleteDialog}>Cancel</Button>
+                <Button color="primary" variant="contained" endIcon={<DeleteIcon />} onClick={deleteBoard}>Delete</Button>
+            </DialogActions>
+        </Dialog>
     </>
     )
 }
